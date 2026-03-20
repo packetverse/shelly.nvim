a5d6ca67e012779cff739ad5e727eae03870abae
packetverse <60551416+packetverse@users.noreply.github.com>
2026-03-20 03:28:04 +0100

    feat(commands): add following commands: ShellyPicker, ShellyTelescope, ShellyListInstalled, ShellyListSupported

--- plugin/shelly.lua
+++ plugin/shelly.lua
@@ -16,6 +16,26 @@ vim.api.nvim_create_user_command("ShellySelect", function()
   shelly.select()
 end, {})
 
+-- alias to ShellySelect
+vim.api.nvim_create_user_command("ShellyPicker", function()
+  shelly.select()
+end, {})
+
+-- telescope picker
+vim.api.nvim_create_user_command("ShellyTelescope", function()
+  vim.cmd("Telescope shelly")
+end, {})
+
 vim.api.nvim_create_user_command("ShellyCurrent", function()
   vim.notify("Current shell: " .. vim.o.shell)
+end, {})
+
+vim.api.nvim_create_user_command("ShellyListInstalled", function()
+  local shells = require("shelly.detection").get_installed_shells()
+  vim.print(shells)
+end, {})
+
+vim.api.nvim_create_user_command("ShellyListSupported", function()
+  local shells = require("shelly.config").known_shells
+  vim.print(shells)
 end, {})
